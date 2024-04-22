7ad3b365c84f38f556ad6921728f3ae33a4ea71e
febits <febits@proton.me>
2024-04-21 21:21:46 -0300

    samples

--- samples/sample.c
+++ samples/sample.c
@@ -5,7 +5,15 @@
 
 int main(void) {
 
-  loguva_test();
+  loguva_add_stream(stdout);
+  loguva_add_file("/tmp/loguva.log");
+  loguva_add_file("/tmp/a.log");
+
+  loguva(DEBUG, "value of var (x): 15");
+  loguva(INFO, "listening on port: 12000");
+  loguva(WARNING, "possible memory leak");
+  loguva(ERROR, "out of bound");
+  loguva(FATAL, "received SIGINT");
 
   return EXIT_SUCCESS;
 }
